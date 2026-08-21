9f4a788118732b37a1986435259078c6b0fe3df1
Mishthi <mishthi.jamini@gmail.com>
2026-08-21 22:53:19 +0530

    Sorting

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -1,25 +1,33 @@
 package Sorting;
 
 public class BubbleSort {
-    public static void main(String[] args) {
-            int[] arr = {64, 34, 25, 12, 22, 11, 90};
-            int n = arr.length;
 
-            for (int i = 0; i < n - 1; i++) {
+    static void bubblesort(int[] arr) {
 
-                for (int j = 0; j < n - i - 1; j++) {
+        int n = arr.length;
 
-                    if (arr[j] > arr[j + 1]) {
+        for (int i = 0; i < n - 1; i++) {
 
-                        int temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                    }
+            for (int j = 0; j < n - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
-            }
-
-            for (int i = 0; i < n; i++) {
-                System.out.print(arr[i] + " ");
             }
         }
     }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 2, 8, 1, 3};
+
+        bubblesort(arr);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
